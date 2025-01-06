c582f75dabf0b0f86719d170a005b9b9d034ab6a
Lucas Ruiz Zapata <lruizap2502@gmail.com>
2025-01-06 07:30:31 +0100

    additional concepts, testing

---
+++ additionalFeatures/testing.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.header("Testing")
+
+"""
+Good development hygiene includes testing your code. 
+
+Automated testing allows you to write higher quality code, faster! Streamlit has a built-in testing framework that let's you build tests easily. 
+
+Use your favorite testing framework to run your tests. We like `pytest`. 
+
+When you test a Streamlit app, you simulate running the app, declare user input, and inspect the results. 
+
+You can use GitHub workflows to automate your tests and get instant alerts about breaking changes. 
+
+Learn more in our guide to App testing.
+"""
+
+
+st.link_button("Testing",
+               "https://docs.streamlit.io/develop/concepts/app-testing")
