0b3fe61f0d835a2955221fb4d0e45e54f340da11
Lucas Ruiz Zapata <lruizap2502@gmail.com>
2025-01-06 07:20:54 +0100

    additional concepts, custom components

---
+++ additionalFeatures/customComponents.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.header("Custom Components")
+
+"""
+If you can't find the right component within the Streamlit library, try out custom components to extend Streamlit's built-in functionality. 
+
+Explore and browse through popular, community-created components in the Components gallery. 
+
+If you dabble in frontend development, you can build your own custom component with Streamlit's components API.
+"""
+
+st.link_button("Components gallery",
+               "https://docs.streamlit.io/develop/concepts/custom-components/intro")
+
+"""
+This will be discussed in more detail in another section dedicated to creating custom components for the library.
+
+This gives a lot of freedom when creating pages, graphics and more content. 
+"""
